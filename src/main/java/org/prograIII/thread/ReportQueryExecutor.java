91a9f86e96e14ba44ebd2d3d9dda154b86eef379
MCambara <mcambaraa@miumg.edu.gt>
2025-04-30 13:17:27 -0600

    Fase final, se agrego el uso del Collector para regions y se añadio una version mejorada de logs utilizados en el proyecto

--- src/main/java/org/prograIII/thread/ReportQueryExecutor.java
+++ src/main/java/org/prograIII/thread/ReportQueryExecutor.java
@@ -16,7 +16,7 @@ public class ReportQueryExecutor implements Runnable {
         this.iso = iso;
     }
 
-    // Ejecuto la consulta para obtener informes por fecha e ISO, y los imprime en la consola
+    // Ejecuta la consulta para recuperar los reportes por fecha e ISO, luego imprímelos en la consola
 
     @Override
     public void run() {
@@ -24,16 +24,17 @@ public class ReportQueryExecutor implements Runnable {
 
         TreeMap<String, ReportModel> reports = service.getReportsByDateAndIso(date, iso);
 
-        System.out.printf("%-30s %-10s %-12s %-12s %-12s %-15s %-20s\n",
-                "Province", "ISO", "Date", "Confirmed", "Deaths", "Recovered", "Region");
+        System.out.printf("%-5s %-30s %-10s %-12s %-12s %-12s %-15s %-20s\n",
+                "No.", "Province", "ISO", "Date", "Confirmed", "Deaths", "Recovered", "Region");
 
         if (reports.isEmpty()) {
             System.out.println("No reports found for date " + date + " and ISO " + iso);
         } else {
-
+            int index = 1;
             for (Map.Entry<String, ReportModel> entry : reports.entrySet()) {
                 ReportModel report = entry.getValue();
-                System.out.printf("%-30s %-10s %-12s %-12d %-12d %-15d %-20s\n",
+                System.out.printf("%-5d %-30s %-10s %-12s %-12d %-12d %-15d %-20s\n",
+                        index++,
                         entry.getKey(),
                         iso,
                         date,
@@ -42,6 +43,7 @@ public class ReportQueryExecutor implements Runnable {
                         report.getRecovered(),
                         report.getRegionName());
             }
+            System.out.println("\nTotal records: " + (index - 1));
         }
     }
 }
